4cbe58a6694c53a22c2be9f1edd29e1b47f2d768
zhanglei91 <1406693536@qq.com>
2018-11-06 11:44:21 +0800

    控制台页面元素

--- page/Console_page.py
+++ page/Console_page.py
@@ -2,6 +2,8 @@ from.common import PageObject, PageElement
 
 """控制台页面元素"""
 class Consolepage(PageObject):
+    """登录控制台"""
+
     # 控制台
     console_table = PageElement(xpath="//li[@id='ConsoleLoginPageId']/a")
     # 密码框
@@ -12,3 +14,10 @@ class Consolepage(PageObject):
     console_close_btn = PageElement(xpath="//a[@class='btn-close']")
     # 断言
     console_assert = PageElement(xpath="//p[contains(text(),'欢迎使用企云会控制台')]")
+
+    """退出控制台"""
+    # 退出按钮
+    console_out = PageElement(xpath="//a[contains(text(),'退出控制台')]")
+    # 断言
+    out_assert = PageElement(xpath="//a[contains(text(),'首页')]")
+
