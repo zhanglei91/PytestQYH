be5d86ecf1210efacadc4af4111b1b0cce230b83
zhanglei91 <1406693536@qq.com>
2018-11-06 09:37:34 +0800

    查看服务协议

--- test_case/test_Termsofservices.py
+++ test_case/test_Termsofservices.py
@@ -1,27 +1,36 @@
+#-*- coding: UTF-8 -*-
 import pytest
 from page.Termsofservice_page import Termsofservicepage
+from page.Login_page import LoginPage
 from page.common import WebDriver
 
-class Termsofservice:
+class TestTermsofservice:
+    """
+    查看协议
+    """
 
     def test_Termsof(self, browser):
-        """查看协议"""
+        """
+        查看服务条款
+        :param browser:
+        :return:
+        """
+        pages = LoginPage(browser)
         page = Termsofservicepage(browser)
-        page.account_bnt.click()
+        page.get("http://pre.yunmeetings.com")
+        pages.account_bnt.click()
+        print("点击账号登录")
         page.register_bnt.click()
+        print("点击注册新账号")
         page.register_num.click()
+        print("手机号注册")
         page.terms_service.click()
-        WebDriver.open_new_window(self, "link_text=《服务条款》")
-        # # 获取默认页面的句柄
-        # default_handle = self.driver.current_window_handle
-        #
-        # # 获取所有句柄
-        # handles = self.driver.window_handles
-        # for handle in handles:
-        #     if handle != default_handle:
-        #         self.driver.switch_to_window(handle)
+        print("服务条款")
+        page.wait(2)
+        page.switch_Windows()
+        print("跳转到服务条款页面")
         success = page.judge.text
         assert success == "企云会服务协议"
 
     if __name__ == '__main__':
-        pytest.main(["-v", "-s", "test_Termsofservices.py::TestTermsofservice",  "--html", "./report.html"])
+        pytest.main(["-v", "-s", "test_Termsofservices.py::TestTermsofservice",  "--html", "../report_test/report.html"])
